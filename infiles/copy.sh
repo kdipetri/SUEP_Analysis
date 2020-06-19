c2f273fb462d5111b8ad7959363304b3dc33a8f8
Karri Folan Di Petrillo <karri.folan.di.petrillo@cern.ch>
2020-06-19 14:53:34 -0500

    setup for local studies for data scouting

---
+++ infiles/copy.sh
@@ -0,0 +1,3 @@
+#scp kdipetri@lpc:/eos/uscms/store/user/kdipetri/SUEP/Production_v0.0/2018/NTUP/PrivateSamples.SUEP_2018_mMed-750_mDark-2_temp-2_decay-generic_13TeV-pythia8_n-100_*_RA2AnalysisTree.root .
+scp kdipetri@lpc:/uscms/home/kdipetri/nobackup/SUEP_analysis/*generic*.root .
+#scp kdipetri@lpc:/uscms/home/kdipetri/nobackup/SUEP_analysis/*.root .
