189b79b6a292c8dcb6073945e43b65229d742046
Kevin Pedro <kpedro88@gmail.com>
2020-06-19 10:13:10 -0500

    adapt scripts to cmssw

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc530/external/gcc/5.3.0/etc/profile.d/init.sh
-#source /cvmfs/cms.cern.ch/slc7_amd64_gcc493/lcg/root/6.06.00/bin/thisroot.sh
-#source /cvmfs/cms.cern.ch/slc7_amd64_gcc530/lcg/root/6.08.07-njopjo/bin/thisroot.sh
-#source /cvmfs/cms.cern.ch/slc7_amd64_gcc530/lcg/root/6.08.07/bin/thisroot.sh
-#source /cvmfs/cms.cern.ch/slc7_amd64_gcc700/lcg/root/6.12.07/bin/thisroot.sh
-source /cvmfs/sft.cern.ch/lcg/views/LCG_97rc4/x86_64-centos7-gcc9-opt/setup.sh
+#!/bin/bash -e
+
+export SCRAM_ARCH=slc7_amd64_gcc700
+# cmd below is cmsrel
+scram project CMSSW_10_6_13
+cd CMSSW_10_6_13/src/
+# cmd below is cmsenv
+eval `scram runtime -sh`
+git clone https://github.com/kdipetri/SUEP_Analysis
+cd SUEP_Analysis
+./compile.sh
