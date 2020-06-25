44a08c41d217d1209bf77427124447798d2f3acb
Karri DiPetrillo <kdipetri@github.com>
2020-06-24 22:01:17 -0500

    fix lpc running

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ filepath=root://cmseos.fnal.gov//store/user/kdipetri/SUEP/Production_v0.0/2018/m
 #./doHistos mMed-1000_mDark-2_temp-2_decay-darkPho_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
 #./doHistos mMed-1000_mDark-2_temp-2_decay-generic_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
 
-./doHistos mMed-125_mDark-2_temp-2_decay-darkPhoHad_13TeV $filepath/SUEP_2018_mMed-125_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
-./doHistos mMed-125_mDark-2_temp-2_decay-generic_13TeV $filepath/SUEP_2018_mMed-125_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
-./doHistos mMed-400_mDark-2_temp-2_decay-darkPhoHad_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
-./doHistos mMed-750_mDark-2_temp-2_decay-darkPhoHad_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
+#./doHistos mMed-125_mDark-2_temp-2_decay-darkPhoHad_13TeV $filepath/SUEP_2018_mMed-125_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
+#./doHistos mMed-125_mDark-2_temp-2_decay-generic_13TeV $filepath/SUEP_2018_mMed-125_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
+#./doHistos mMed-400_mDark-2_temp-2_decay-darkPhoHad_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
+#./doHistos mMed-750_mDark-2_temp-2_decay-darkPhoHad_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
 
 #./doHistos mMed-125_mDark-2_temp-2_decay-darkPho_13TeV $filepath/SUEP_2018_mMed-125_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
 #./doHistos mMed-400_mDark-2_temp-2_decay-darkPho_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
@@ -16,10 +16,10 @@ filepath=root://cmseos.fnal.gov//store/user/kdipetri/SUEP/Production_v0.0/2018/m
 #./doHistos mMed-750_mDark-2_temp-2_decay-darkPho_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
 #./doHistos mMed-750_mDark-2_temp-2_decay-generic_13TeV $filepath/SUEP_2018_mMed-1000_mDark-2_temp-2_decay-darkPhoHad_13TeV-pythia8_AnalysisTree.root
 
-#./doHistos QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM
-#./doHistos QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM
-#./doHistos QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM
-#./doHistos QCD_HT200to300_TuneCP5_13TeV-madgraphMLM
-#./doHistos QCD_HT300to500_TuneCP5_13TeV-madgraphMLM
-#./doHistos QCD_HT500to700_TuneCP5_13TeV-madgraphMLM
-#./doHistos QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM
+./doHistos QCD_HT1000to1500 $filepath/QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8_AnalysisTree.root
+./doHistos QCD_HT1500to2000 $filepath/QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8_AnalysisTree.root
+./doHistos QCD_HT2000toInf $filepath/QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8_AnalysisTree.root
+./doHistos QCD_HT200to300 $filepath/QCD_HT200to300_TuneCP5_13TeV-madgraphMLM-pythia8_AnalysisTree.root
+./doHistos QCD_HT300to500 $filepath/QCD_HT300to500_TuneCP5_13TeV-madgraphMLM-pythia8_AnalysisTree.root
+./doHistos QCD_HT500to700 $filepath/QCD_HT500to700_TuneCP5_13TeV-madgraphMLM-pythia8_AnalysisTree.root
+./doHistos QCD_HT700to1000 $filepath/QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8_AnalysisTree.root
